750547e09b41c15f021cfc91fa94f7bfb2f8cc50
Anish g <anishg@Anishs-MacBook-Air.local>
2026-07-31 20:13:28 +0530

    Fix build type error in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-    buildActivityPosition: "bottom-right",
-  },
+
   allowedDevOrigins: [
     'compiler-innocent-regression-pharmacology.trycloudflare.com',
     'skating-specialist-alarm-without.trycloudflare.com',
